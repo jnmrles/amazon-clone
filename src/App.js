4b598abb01732ce079d40a3bfa06eb78f60a7061
JM <jnmrles@gmail.com>
2020-12-27 23:39:10 -0600

    added home component with product row components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Header from './Header.js'
+import Home from './Home.js'
+
+// This is where you add your main components. Home page
 function App() {
   return (
     // BEM Convention for app
     <div className="app">
      <Header/>
+     <Home/>
      {/*Header */}
       {/*Home */}
     </div>
