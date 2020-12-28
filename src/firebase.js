e430137da2a6f3f27f77ee8581f589d610e44b29
JM <jnmrles@gmail.com>
2020-12-27 19:35:12 -0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcFGzwyKgGueTVMtuzVunJKukVbcPQvnk",
+  authDomain: "fir-c271c.firebaseapp.com",
+  projectId: "fir-c271c",
+  storageBucket: "fir-c271c.appspot.com",
+  messagingSenderId: "265644791320",
+  appId: "1:265644791320:web:c0a5925b15619bb156956d"
+};
